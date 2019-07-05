5a34eede8ee399b22937102404b3aa72646c62d4
Jacinto Palma <jxcinto12@gmail.com>
2019-07-05 11:46:31 -0500

    Index Page - CSS

--- TurisHuma/api-web/src/app.controller.ts
+++ TurisHuma/api-web/src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,Response } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('inicio')
+  indexPage(@Response() res){
+    res.render('inicio',{});
   }
 }
--- TurisHuma/api-web/src/main.ts
+++ TurisHuma/api-web/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cookieParser = require('cookie-parser');
+import {NestExpressApplication} from "@nestjs/platform-express";
+import {join} from 'path';
+import * as express from 'express';
+import * as path from 'path';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule) as NestExpressApplication;
+
+  app.set('view engine','ejs');
+  app.setBaseViewsDir(join(__dirname,'..','views'));
+  await app.listen(3002);
+
 }
 bootstrap();
